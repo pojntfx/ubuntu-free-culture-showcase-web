f382185c43b5a3f769777eaacb98dfa8956e5664
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-05-27 18:06:21 +0200

    Use cards instead of img + card

--- src/components/start/FeedCollection.js
+++ src/components/start/FeedCollection.js
@@ -1,25 +1,56 @@
 import React from "react";
-import { List, ListItem, Image } from "vanilla-framework-react";
+import { List, ListItem, Card } from "vanilla-framework-react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 export const FeedCollection = () => (
   <List modifier="split">
-    <PreviewImage src="http://placekitten.com/g/200/200" />
-    <PreviewImage src="http://placekitten.com/g/234/200" />
-    <PreviewImage src="http://placekitten.com/g/500/200" />
-    <PreviewImage src="http://placekitten.com/g/356/325" />
-    <PreviewImage src="http://placekitten.com/g/243/200" />
+    <SubmissionCard src="https://picsum.photos/500/500" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/234/200" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/500/200" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/356/325" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/243/200" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/455/200" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/345/678" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/455/456" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/564/456" title="Submission" />
+    <SubmissionCard src="https://picsum.photos/678/450" title="Submission" />
   </List>
 );
 
-const PreviewImage = ({ src }) => (
-  <a href={src}>
+const PreviewImage = ({ src, title, ...otherProps }) => (
+  <a href={src} {...otherProps}>
     <ListItem>
-      <Image bordered shadowed src={src} style={{ width: "100%" }} />
+      <Card
+        title={title}
+        image={{
+          src,
+          alt: "Submission Image",
+          style: { minWidth: "100%" }
+        }}
+      >
+        {""}
+      </Card>
     </ListItem>
   </a>
 );
 
+const SubmissionCard = styled(PreviewImage)`
+  .p-card {
+    margin-bottom: 0 !important;
+    header {
+      img {
+        width: 100%;
+      }
+    }
+    h3 {
+      margin: 0 !important;
+      padding-bottom: 0.1rem;
+    }
+  }
+`;
+
 PreviewImage.propTypes = {
-  src: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
