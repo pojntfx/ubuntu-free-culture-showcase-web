d45fea99002c85a82448c19d96ee4b660b6c289b
Felix Pojtinger <felix@pojtinger.com>
2018-05-27 20:23:57 +0200

    Replaced Header with Message on Details page, added less random names for mock data

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -5,57 +5,57 @@ import { CurrentHeader } from "../global/Header";
 import { ImageCollection, SubmissionCard } from "./ImageCollection";
 
 export const Start = () => (
-  <DefaultLayout header={<CurrentHeader fullHeight/>}>
+  <DefaultLayout header={<CurrentHeader fullHeight />}>
     <SearchBox />
     <ImageCollection>
       <SubmissionCard
         src="https://picsum.photos/500/500"
-        title="Submission"
+        title="The Mist"
         id="1"
       />
       <SubmissionCard
         src="https://picsum.photos/234/200"
-        title="Submission"
+        title="Tropical"
         id="2"
       />
       <SubmissionCard
         src="https://picsum.photos/500/200"
-        title="Submission"
+        title="The North See"
         id="3"
       />
       <SubmissionCard
         src="https://picsum.photos/356/325"
-        title="Submission"
+        title="Space"
         id="4"
       />
       <SubmissionCard
         src="https://picsum.photos/243/200"
-        title="Submission"
+        title="Industrial"
         id="5"
       />
       <SubmissionCard
         src="https://picsum.photos/455/200"
-        title="Submission"
+        title="Scandinavia"
         id="6"
       />
       <SubmissionCard
         src="https://picsum.photos/345/678"
-        title="Submission"
+        title="Urban"
         id="7"
       />
       <SubmissionCard
         src="https://picsum.photos/455/456"
-        title="Submission"
+        title="Trees"
         id="8"
       />
       <SubmissionCard
         src="https://picsum.photos/564/456"
-        title="Submission"
+        title="Human Energy"
         id="9"
       />
       <SubmissionCard
         src="https://picsum.photos/678/450"
-        title="Submission"
+        title="A Wall"
         id="10"
       />
     </ImageCollection>
